bd939d3b40f6f5b5973cd23177056634817723bb
Jack Arturo <info@verygoodplugins.com>
2026-02-21 00:54:17 +0530

    chore(viewer): remove vendored graph-viewer after repo split

--- packages/graph-viewer/src/components/settings/index.ts
+++
@@ -1,4 +0,0 @@
-export { SettingsPanel } from './SettingsPanel'
-export { SettingsSection } from './SettingsSection'
-export { SliderControl } from './SliderControl'
-export { ToggleControl } from './ToggleControl'
