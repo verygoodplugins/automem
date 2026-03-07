d60366789758416344c31bff9dec0bc0f8a83b48
Jack Arturo <info@verygoodplugins.com>
2026-03-08 00:03:56 +0530

    test: add return annotations to recall entity extraction tests

--- tests/test_recall_entity_extraction.py
+++ tests/test_recall_entity_extraction.py
@@ -1,11 +1,11 @@
 from automem.api.recall import _extract_query_entities
 
 
-def test_extract_query_entities_handles_ascii_possessive_name():
+def test_extract_query_entities_handles_ascii_possessive_name() -> None:
     entities = _extract_query_entities("Would Caroline's sister pursue writing as a career?")
     assert "Caroline" in entities
 
 
-def test_extract_query_entities_handles_curly_possessive_name():
+def test_extract_query_entities_handles_curly_possessive_name() -> None:
     entities = _extract_query_entities("Would Caroline’s sister pursue writing as a career?")
     assert "Caroline" in entities
